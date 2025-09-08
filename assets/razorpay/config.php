cb88b54dfe5f522365dce0baf6d53da188f1ab06
aavirbhav2025 <aavirbhav2025@gmail.com>
2025-09-08 16:27:08 +0530

    ..

--- assets/razorpay/config.php
+++ assets/razorpay/config.php
@@ -11,4 +11,4 @@ define('WEBHOOK_SECRET', 'set_a_random_long_string_here');
 
 // Composer autoload for Razorpay PHP SDK
 //   composer require razorpay/razorpay:^2
-require __DIR__ . '/vendor/autoload.php';
+require _DIR_ . '/vendor/autoload.php';
