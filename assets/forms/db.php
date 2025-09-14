0f91edb1c98e3d78b7f007b0afe44ce59565febd
Harikiran <harikiranap123@gmail.com>
2025-09-14 10:08:08 +0530

    ...

--- assets/forms/db.php
+++ assets/forms/db.php
@@ -1,20 +1,8 @@
 <?php
-// Detect if running locally or live
-if ($_SERVER['HTTP_HOST'] === 'localhost') {
-    // Localhost (XAMPP/WAMP)
-    $servername = "localhost";
-    $username   = "root"; // default in XAMPP
-    $password   = "";     // default password is empty
-    $dbname     = "aavirbhav"; // your local database
-
-} else
- {
-    // Live server credentials
-    $servername = "localhost";
-    $username   = "u223469901_aavirbhav";
-    $password   = "Ox^j[oix*5";
-    $dbname     = "u223469901_aavirbhav";
-}
+$servername = "localhost";
+$username   = "aavi_aavi_aavirbhav";
+$password   = "YeT3!New85f7g7*^";
+$dbname     = "aavi_aavi_aavirbhav";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
